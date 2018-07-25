e9e27b82c629284bd1b7f321f986ed36de6e4f87
Toshiyuki FUJITA <kalibora@gmail.com>
2018-07-25 20:09:22 +0900

    support closure

--- src/ConsoleProcessor.php
+++ src/ConsoleProcessor.php
@@ -12,6 +12,14 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 class ConsoleProcessor implements EventSubscriberInterface
 {
     protected $command;
+    protected $input;
+    protected $output;
+    protected $closure;
+
+    public function __construct(callable $closure = null)
+    {
+        $this->closure = $closure;
+    }
 
     /**
      * @param array $record
@@ -20,10 +28,30 @@ class ConsoleProcessor implements EventSubscriberInterface
      */
     public function __invoke(array $record)
     {
+        $extra = [];
+
         if ($this->command) {
             $name = $this->command->getName();
+            $extra['command_name'] = $name;
+        }
 
-            $record['extra']['command_name'] = $name;
+        if ($this->closure) {
+            $other = call_user_func($this->closure, $this->command, $this->input, $this->output);
+            if (!is_array($other)) {
+                throw new \DomainException('Must return array');
+            }
+
+            $extra = array_merge($extra, $other);
+        }
+
+        if (empty($extra)) {
+            return $record;
+        }
+
+        if (isset($record['extra'])) {
+            $record['extra'] = array_merge($record['extra'], $extra);
+        } else {
+            $record['extra'] = $extra;
         }
 
         return $record;
@@ -35,6 +63,8 @@ class ConsoleProcessor implements EventSubscriberInterface
     public function onCommand(ConsoleCommandEvent $event)
     {
         $this->command = $event->getCommand();
+        $this->input = $event->getInput();
+        $this->output = $event->getOutput();
     }
 
     /**
--- tests/ConsoleProcessorTest.php
+++ tests/ConsoleProcessorTest.php
@@ -3,6 +3,10 @@
 namespace Otobank\Monolog\Processor;
 
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Event\ConsoleCommandEvent;
 
 class ConsoleProcessorTest extends TestCase
 {
@@ -14,10 +18,21 @@ class ConsoleProcessorTest extends TestCase
         $processor = new ConsoleProcessor();
 
         if ($commandName) {
-            $commandp = $this->prophesize('\Symfony\Component\Console\Command\Command');
+            // Command
+            $commandp = $this->prophesize(Command::class);
             $commandp->getName()->willReturn($commandName);
-            $eventp = $this->prophesize('\Symfony\Component\Console\Event\ConsoleCommandEvent');
+
+            // InputInterface
+            $inputp = $this->prophesize(InputInterface::class);
+
+            // OutputInterface
+            $outputp = $this->prophesize(OutputInterface::class);
+
+            // Event
+            $eventp = $this->prophesize(ConsoleCommandEvent::class);
             $eventp->getCommand()->willReturn($commandp->reveal());
+            $eventp->getInput()->willReturn($inputp->reveal());
+            $eventp->getOutput()->willReturn($outputp->reveal());
 
             $processor->onCommand($eventp->reveal());
         }
@@ -25,7 +40,7 @@ class ConsoleProcessorTest extends TestCase
         $record = [];
         $record = $processor($record);
 
-        $this->assertSame($expectedRecord, $record);
+        $this->assertEquals($expectedRecord, $record);
     }
 
     public function providerForInvoke()
@@ -33,6 +48,56 @@ class ConsoleProcessorTest extends TestCase
         return [
             [null,  []],
             ['foo', ['extra' => ['command_name' => 'foo']]],
+        ];
+    }
+
+    /**
+     * @dataProvider providerForInvokeWithClosure
+     */
+    public function testInvokeWithClosure($commandName, $expectedRecord)
+    {
+        $processor = new ConsoleProcessor(function ($command, $input, $output) {
+            return [
+                'command_hoge' => 'fuga',
+                'input_bar' => $input->getArgument('bar'),
+            ];
+        });
+
+        // Command
+        $commandp = $this->prophesize(Command::class);
+        $commandp->getName()->willReturn($commandName);
+
+        // InputInterface
+        $inputp = $this->prophesize(InputInterface::class);
+        $inputp->getArgument('bar')->willReturn('baz');
+
+        // OutputInterface
+        $outputp = $this->prophesize(OutputInterface::class);
+
+        // Event
+        $eventp = $this->prophesize(ConsoleCommandEvent::class);
+        $eventp->getCommand()->willReturn($commandp->reveal());
+        $eventp->getInput()->willReturn($inputp->reveal());
+        $eventp->getOutput()->willReturn($outputp->reveal());
+
+        $processor->onCommand($eventp->reveal());
+
+        $record = [];
+        $record = $processor($record);
+
+        $this->assertEquals($expectedRecord, $record);
+    }
+
+    public function providerForInvokeWithClosure()
+    {
+        return [
+            ['foo', [
+                'extra' => [
+                    'input_bar' => 'baz',
+                    'command_name' => 'foo',
+                    'command_hoge' => 'fuga',
+                ],
+            ]],
         ];
     }
 }
